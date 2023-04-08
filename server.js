3acb6d08349b3ca1036f5e2b26c0e63162f88c04
Zlatelina Rizova <zlatelinarizova@gmail.com>
2023-04-08 14:55:18 -0500

    adding a route and path to the server

--- server.js
+++ server.js
@@ -10,3 +10,13 @@ const port = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+
+//-- GET route for the root path of the server--//
+//-- Adding a  two functions (req and res), which represent the incoming request--//
+//--Inside the callback function, the res.sendFile() method is used to send the index.html file to the client--//
+app.get("/", (req, res) => {
+    //-- Adding a index.html file to the client as a response to a GET request to the root path of the server ("/")--//
+      //-- The res.sendFile() method is an Express.js method that sends a file as the response to a client's request--//
+      //--The argument passed to sendFile() is the file path of the file to be sent--//
+      res.sendFile(__dirname + "/public/index.html");
+    });
