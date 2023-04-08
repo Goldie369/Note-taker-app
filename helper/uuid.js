9ab1c2a27e16881de87ff1cb071e19e45389c2f1
Zlatelina Rizova <zlatelinarizova@gmail.com>
2023-04-08 15:01:08 -0500

    adding a comments

--- helper/uuid.js
+++ helper/uuid.js
@@ -1,3 +1,5 @@
+//-- Adding a module.exports = () => ... function that generates a random hexadecimal string--//
+//--This function can be imported and used in other parts of the code--//
 module.exports = () =>
   Math.floor((1 + Math.random()) * 0x10000)
     .toString(16)
