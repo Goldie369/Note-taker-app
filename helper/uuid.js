890fd18eb5d4c52c375ea5d5d855a502be087424
Zlatelina Rizova <zlatelinarizova@gmail.com>
2023-04-11 19:17:52 -0500

    modifying the helper file

--- helper/uuid.js
+++ helper/uuid.js
@@ -1,7 +1,10 @@
 //-- Adding a module.exports = () => ... function that generates a random hexadecimal string--//
 //--This function can be imported and used in other parts of the code--//
 
-module.exports = () =>
-  Math.floor((1 + Math.random()) * 0x10000)
-    .toString(16)
-    .substring(1);
+    uuid = () => {
+      var num = Math.floor((1 + Math.random()) * 0x10000)
+      num.toString(16)
+      num.substring(1);
+      return num
+    }
+    module.exports = uuid 
